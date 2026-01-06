989a854bfd03db4e51f125d4d7a4117b67625e1f
Pushpendra Kumar <pushpendrakumar@Pushpendras-MacBook-Air.local>
2026-01-07 00:09:59 +0530

    Added more pattern logic

--- Basic/starPattern.java
+++ Basic/starPattern.java
@@ -5,8 +5,41 @@ public class starPattern {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+
+        // *
+        // * *
+        // * * *
+        // * * * * 
         for (int i = 1; i <= n; i++) {          // line number
             for (int j = 1; j <= i; j++) {      // print i stars
+                System.out.print("*");
+            }
+            System.out.println();               // move to next line
+        }
+
+
+        // * * * *
+        // * * *
+        // * *
+        // *
+ 
+        for (int i = n; i >= 1; i--) {         // line number
+            for (int j = 1; j <= i; j++) {     // print i stars
+                System.out.print("*");      
+            }
+            System.out.println();               // move to next line
+        }
+
+
+        //     *
+        //    **
+        //   ***
+        //  ****
+        for (int i = 1; i <= n; i++) {          // line number
+            for (int j = 1; j <= n - i; j++) {  // print spaces
+                System.out.print(" ");  
+            }
+            for (int k = 1; k <= i; k++) {      // print i stars
                 System.out.print("*");
             }
             System.out.println();               // move to next line
